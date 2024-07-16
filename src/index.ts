8e6d71454bab6d22bff89aac7578e407954a52f5
NicoGGG <coolranger@protonmail.com>
2024-07-16 23:30:32 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+import * as ejs from 'ejs';
+import { promises as fs } from 'fs';
+import { command } from 'execa';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { join } from 'path';
+
+interface Args {
+  project: string;
+}
+
+(async () => {
+  const templates = [
+    'index.ts',
+    'package.json',
+    'tsconfig.json',
+    'tsconfig.build.json',
+    '.gitignore',
+    '.prettierrc',
+    '.eslintrc.js',
+  ];
+  const templatesDir = join(__dirname, 'templates');
+  try {
+    const argv = yargs(hideBin(process.argv))
+      .option('project', {
+        alias: 'p',
+        describe: 'Name of the project',
+        type: 'string',
+        demandOption: true,
+      })
+      .help()
+      .alias('help', 'h')
+      .parseSync() as Args;
+
+    console.log(`Creating project ${argv.project}`);
+
+    if (!argv.project) {
+      throw new Error('Project name is required.');
+    }
+
+    const renderedFiles = new Array<Promise<string>>();
+    templates.forEach((file) => {
+      renderedFiles.push(
+        ejs.renderFile(`${templatesDir}/${file}`, {
+          name: argv.project,
+        }),
+      );
+    });
+
+    await fs.mkdir(`${argv.project}/src`, { recursive: true });
+    const files = await Promise.all(renderedFiles);
+
+    files.forEach(async (file, index) => {
+      await fs.writeFile(`${argv.project}/${templates[index]}`, file);
+    });
+    console.log('Files written successfully.');
+    await fs.rename(`${argv.project}/index.ts`, `${argv.project}/src/index.ts`);
+    console.log('Installing dependencies...');
+    const cmd = 'npm install --verbose';
+
+    await command(cmd, { stdio: 'ignore', cwd: argv.project });
+
+    console.log('Template rendered and written to file successfully.');
+  } catch (err) {
+    console.error('Error rendering template or writing to file:', err);
+  }
+})();
